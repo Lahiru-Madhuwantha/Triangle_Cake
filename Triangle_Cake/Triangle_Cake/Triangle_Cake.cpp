c263f4603f210d025af3d1df0bae458dcc5783e4
Lahiru-Madhuwantha <136086327+Lahiru-Madhuwantha@users.noreply.github.com>
2023-06-16 00:11:59 +0530

    Add files via upload

---
+++ Triangle_Cake/Triangle_Cake/Triangle_Cake.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include<map>
+
+using namespace std;
+
+int main()
+{
+    int l,w;
+    cin >> l;
+    cin >> w;
+    float A;
+    A = 0.5 * l * w;
+    cout << A << endl;
+
+
+}
+
